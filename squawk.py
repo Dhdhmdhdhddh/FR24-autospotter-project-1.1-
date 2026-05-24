75dafae73d442c8c9cb956bbd9e5e2cb589a4734
Dhdhmdhdhddh <berenjinali@yahoo.com>
2026-05-24 13:18:32 -0500

    Update squawk.py

--- squawk.py
+++ squawk.py
@@ -9,7 +9,7 @@ import time
 import logging
 import requests
 from datetime import datetime, timezone
-from FlightRadar24 import FlightRadar24API
+from FlightRadarAPI import FlightRadar24API
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 log = logging.getLogger(__name__)
